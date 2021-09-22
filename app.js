4579d2f4fe5fafa3bf4f65d5b218de025c16eafa
AlQasem  M. Jawad <kssma19942000@gmail.com>
2021-09-23 02:41:20 +0300

    testing

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-let car = {
+let add = (num1, num2)=>{
+    if (num1<0 || num2<0) {
+        return "Negitive number"
+    }
+    else {
+        return num1 + num2
+    }
+}
 
-    color : "Red",
-    
-    brand: "BMW",
-    
-    'make date' : 1990,
-    
-    engine : {
-    
-    Size : 3500,
-    
-    Cylinders : 6,
-    
-    Valve : 16 
-    
-    }
-    
-    }
+let result = add(5,6)
 
-    for(key in car.engine) {
-        console.log(key, car.engine[key])
-    }
+console.log(result)
